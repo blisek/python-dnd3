0e87c5fe4f55c53aa0fe9b81d5fd8b7649c2cfa8
blisek <bartlomiej.lisek@hotmail.com>
2015-05-02 23:01:43 +0200

    flags

---
+++ dnd3/flags.py
@@ -0,0 +1,24 @@
+__author__ = 'bartek'
+
+A_LG = 'lawful_good'
+A_LN = 'lawful_neutral'
+A_LE = 'lawful_evil'
+A_NG = 'neutral_good'
+A_N = 'neutral'
+A_NE = 'neutral_evil'
+A_CG = 'chaotic_good'
+A_CN = 'chaotic_neutral'
+A_CE = 'chaotic_evil'
+
+ALIGNMENT_FLAGS = {
+    A_LG: 1,
+    A_LN: 2,
+    A_LE: 4,
+    A_NG: 8,
+    A_N: 16,
+    A_NE: 32,
+    A_CG: 64,
+    A_CN: 128,
+    A_CE: 256
+}
+
