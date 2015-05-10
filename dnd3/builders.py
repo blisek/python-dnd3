6382a495afb41450390305917987d8a5e0c7c30a
blisek <bartlomiej.lisek@hotmail.com>
2015-05-10 20:44:10 +0200

    poprawki 2

--- dnd3/builders.py
+++ dnd3/builders.py
@@ -5,14 +5,23 @@ class CreatureBuilder:
     def __init__(self):
         pass
 
-    def assign_race(self):
+    def assign_before(self, **kwargs):
+        pass
+
+    def assign_race(self, **kwargs):
         raise NotImplementedError()
 
-    def assign_class(self):
+    def assign_class(self, **kwargs):
         raise NotImplementedError()
 
-    def assign_skills(self):
+    def assign_skills(self, **kwargs):
         raise NotImplementedError()
 
-    def assign_feats(self):
+    def assign_feats(self, **kwargs):
         raise NotImplementedError()
+
+    def assign_after(self, **kwargs):
+        pass
+
+    def build(self):
+        pass
